a568470dad53da4807ea2920c12a54545ee948b4
dankton96 <dankton96@hotmail.com>
2017-04-15 12:17:48 -0300

    in development

--- app/src/main/java/com/example/root/challenge/Imagens.java
+++ app/src/main/java/com/example/root/challenge/Imagens.java
@@ -1,10 +1,15 @@
 package com.example.root.challenge;
 
 
+import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
+
 public class Imagens
     {
         String autor;
         String fonte;
         String legenda;
         String url;
+        ImageView Show = new ImageView();
     }
